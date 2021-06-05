fdfec2cb1dd043cd19a7ba7082c5e1cb7642bc28
A.M Darshan <amdarshan.2001@gmail.com>
2021-06-05 13:21:21 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,15 @@
+package LetsLearnGit;
+import java.util.*;
+public class Sum{
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc= new Scanner(System.in);
+        a= sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+        
+    }
+}
